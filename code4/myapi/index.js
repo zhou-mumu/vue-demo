b9acfb82c4d7cc5f11fbe448243cbf313487df31
zhoumumu <953474446@qq.com>
2021-02-07 22:21:42 +0800

    Vue

--- code4/myapi/index.js
+++ code4/myapi/index.js
@@ -12,8 +12,7 @@ app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Headers', 'mytoken');
+  res.header('Access-Control-Allow-Headers', ['mytoken','Content-Type']);
   next();
 });
 app.get('/async1', (req, res) => {
